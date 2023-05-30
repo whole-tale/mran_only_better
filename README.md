88055fbcebad8170c76d3e2f2fa01b97503a3cbc
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2023-05-30 11:55:17 -0500

    Initial import

---
+++ README.md
@@ -0,0 +1,58 @@
+# Better Than MRAN^{TM}
+
+A very simple http server that serves /snapshot/{date} for getting R source packages for a given date straight from
+CRAN.
+
+## Example usage from R
+
+```
+> options(repos = c(CRAN = "http://localhost:8000/snapshot/2021-12-01"))
+> install.packages('ctv')
+Installing package into '/usr/local/lib/R/site-library'
+(as 'lib' is unspecified)
+trying URL 'http://localhost:8000/snapshot/2021-12-01/src/contrib/ctv_0.8-5.tar.gz'
+downloaded 355 KB
+
+* installing *source* package 'ctv' ...
+** package 'ctv' successfully unpacked and MD5 sums checked
+** using staged installation
+** R
+** inst
+** byte-compile and prepare package for lazy loading
+** help
+*** installing help indices
+** building package indices
+** installing vignettes
+** testing if installed package can be loaded from temporary location
+** testing if installed package can be loaded from final location
+** testing if installed package keeps a record of temporary installation path
+* DONE (ctv)
+
+The downloaded source packages are in
+	'/tmp/Rtmp4fRWmH/downloaded_packages'
+> options(repos = c(CRAN = "http://localhost:8000/snapshot/2022-12-01"))
+> install.packages('ctv')
+Installing package into '/usr/local/lib/R/site-library'
+(as 'lib' is unspecified)
+trying URL 'http://localhost:8000/snapshot/2022-12-01/src/contrib/ctv_0.9-4.tar.gz'
+downloaded 85 KB
+
+* installing *source* package 'ctv' ...
+** package 'ctv' successfully unpacked and MD5 sums checked
+** using staged installation
+** R
+** inst
+** byte-compile and prepare package for lazy loading
+** help
+*** installing help indices
+*** copying figures
+** building package indices
+** installing vignettes
+** testing if installed package can be loaded from temporary location
+** testing if installed package can be loaded from final location
+** testing if installed package keeps a record of temporary installation path
+* DONE (ctv)
+
+The downloaded source packages are in
+	'/tmp/Rtmp4fRWmH/downloaded_packages'
+```
---
+++ app.py
@@ -0,0 +1,73 @@
+# A simple fastAPI app that takes a snapshot date and
+# fetches proper source from CRAN
+import pyreadr
+import os
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import StreamingResponse
+import datetime
+import requests
+import uvicorn
+
+app = FastAPI()
+if not os.path.exists("cran.toc.rds"):
+    print("Downloading cran.toc.rds")
+    r = requests.get("https://groundhogr.com/cran.toc.rds", allow_redirects=True)
+    open("cran.toc.rds", "wb").write(r.content)
+CRAN_TOC = pyreadr.read_r("cran.toc.rds")[None]
+
+# CORS
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
+
+
+async def get_snapshot(date: str):
+    date = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+    result = CRAN_TOC[CRAN_TOC["Published"] <= date]
+    last_date = result.groupby("Package")["Published"].max()
+    result = result[
+        result.apply(lambda row: row["Published"] == last_date[row["Package"]], axis=1)
+    ]
+
+    # iterate over rows with iterrows()
+    # and print each row if it's not None
+    # This is slow as hell, but it works
+    for index, row in result.iterrows():
+        for col in result.columns:
+            if row[col]:
+                yield f"{col}: {row[col]}\n"
+        yield "\n"
+
+
+@app.get("/snapshot/{date}/src/contrib/{filename}")
+async def get_file(date: str, filename: str):
+    if filename.startswith("PACKAGES"):
+        if filename != "PACKAGES":
+            raise HTTPException(status_code=404, detail="File not found")
+        return StreamingResponse(get_snapshot(date), media_type="text/plain")
+
+    # check if filename exists at https://cran.r-project.org/src/contrib/
+    # and if not try the Archive
+    base_url = "https://cran.r-project.org/src/contrib/"
+    for url in [
+        base_url,
+        f"{base_url}Archive/{os.path.basename(filename).rsplit('_')[0]}/",
+    ]:
+        r = requests.get(url + filename, allow_redirects=True, stream=True)
+        print(r.status_code, r.url)
+        if r.status_code == 200:
+            return StreamingResponse(r.raw, media_type="application/x-gzip")
+    else:
+        raise HTTPException(status_code=404, detail="File not found")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
